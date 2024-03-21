af031656b6612c61ca67e611042e86eb8dfb7939
Maryam Chrs <maryam.chrs@gmail.com>
2024-03-21 18:50:30 +0330

    Remove not used fonts

--- ReverseTicTacToe/Common/Extensions/Font+extension.swift
+++ ReverseTicTacToe/Common/Extensions/Font+extension.swift
@@ -8,18 +8,6 @@
 import SwiftUI
 
 extension Font {
-    static func moody(ofSize size: CGFloat) -> Font {
-        return Font.custom("Moody", size: size)
-    }
-    
-    static func sweety(ofSize size: CGFloat) -> Font {
-        return Font.custom("Sweety", size: size)
-    }
-    
-    static func cupcake(ofSize size: CGFloat) -> Font {
-        return Font.custom("Cupcake", size: size)
-    }
-    
     static func sfPro(ofSize size: CGFloat) -> Font {
         return Font.custom("SF Pro Display", size: size)
     }
